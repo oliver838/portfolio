bdd75c2fd7934516deb6a6146184c8cb45cf579a
oliver838 <146178685+oliver838@users.noreply.github.com>
2025-03-20 10:58:02 +0100

    Add files via upload

---
+++ getData.js
@@ -0,0 +1,10 @@
+const getData=async (url,renderFn)=>{
+    try {
+        const response=await fetch(url)
+        const data=await response.json()
+        renderFn(data)
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
